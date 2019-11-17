afb771466b96f7b0c71930b5ebac15fb64382fe9
David Ludlow <dludlow7@ford.com>
2019-11-17 21:07:17 +0000

    Skeleton for Character & fetching character logic

---
+++ MarvelMVC/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  MarvelMVC
+//
+//  Created by dludlow7 on 17/11/2019.
+//  Copyright © 2019 David Ludlow. All rights reserved.
+//
+
+import Foundation
+
+struct Character {
+    
+}
--- MarvelMVC/ListViewController.swift
+++ MarvelMVC/ListViewController.swift
@@ -12,12 +12,12 @@ class ListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Characters"
+    }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    func fetchCharacters() -> [Character] {
+        return [Character()]
     }
 
     // MARK: - Table view data source
