1955c9daa7e3a2c2f238f26af93c10ac806bb2c7
David Ludlow <dludlow7@ford.com>
2019-11-20 22:09:33 +0000

    Now fetching Characters successfully from API

--- MarvelMVC/ListViewController.swift
+++ MarvelMVC/ListViewController.swift
@@ -10,14 +10,61 @@ import UIKit
 
 class ListViewController: UITableViewController {
 
+    var characters = [Character]() {
+        didSet {
+            DispatchQueue.main.async {
+                tableView.reloadData()
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Characters"
+        fetchCharacters()
     }
 
-    func fetchCharacters() -> [Character] {
-        return [Character()]
+    func fetchCharacters() {
+        let url = URL(string: "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=ff3d4847092294acc724123682af904b&hash=412b0d63f1d175474216fadfcc4e4fed&limit=25&orderBy=-modified")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { jsonData, response, error in
+            if let error = error {
+                self.handleClientError(error)
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                (200...299).contains(httpResponse.statusCode) else {
+                    self.handleServerError(response!)
+                    return
+            }
+            if let mimeType = httpResponse.mimeType, mimeType == "application/json",
+                let jsonData = jsonData {
+
+                let decoder = JSONDecoder()
+                let characterResponse = try! decoder.decode(CharacterAPIResponse.self, from: jsonData)
+                self.characters = characterResponse.data?.results ?? []
+            }
+        }
+        task.resume()
+    }
+
+    func handleClientError(_ error: Error) {
+        let ac = UIAlertController(title: "Error", message: "Cannot fetch character list (Client Error)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.async {
+            self.present(ac, animated: true)
+        }
+        print(error)
+    }
+
+    func handleServerError(_ response: URLResponse) {
+        let ac = UIAlertController(title: "Error", message: "Cannot fetch character list (Server Error)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.async {
+            self.present(ac, animated: true)
+        }
+        print(response)
     }
 
     // MARK: - Table view data source
@@ -29,18 +76,14 @@ class ListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return characters.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+//
+//        return cell
+//    }
 
     /*
     // Override to support conditional editing of the table view.
